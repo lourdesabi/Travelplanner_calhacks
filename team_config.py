ca86e47d9228279bf60f121761964d5147135bdc
bryanba25-tech <bryanba25@berkeley.edu>
2025-10-25 17:32:29 -0700

    Added theme selector and team customization config

---
+++ team_config.py
@@ -0,0 +1,19 @@
+class TeamConfig:
+    # Change these to customize the app!
+    APP_NAME = "Your App Name Here"
+    TEAM_NAME = "Your Team Name"
+    TAGLINE = "Your tagline here"
+    
+    # Pick your default theme
+    DEFAULT_THEME = "Purple Gradient"  # Options: Purple Gradient, Ocean Blue, Sunset Orange, Forest Green, Pink Dreams, Midnight Dark
+    
+    # Toggle features on/off
+    SHOW_BOOKING_LINKS = True
+    SHOW_WEATHER = True
+    SHOW_FLIGHTS = True
+    SHOW_METRICS = True
+    
+    # Change defaults
+    DEFAULT_BUDGET = 2000
+    DEFAULT_PASSENGERS = 2
+    DEFAULT_DAYS = 7
