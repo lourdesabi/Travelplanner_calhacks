9867e775ac2cf25648f163f32a8f4637681f64e6
bryanba25-tech <bryanba25@berkeley.edu>
2025-10-26 05:20:50 -0700

    Simplify build.sh - don't remove venv

--- build.sh
+++ build.sh
@@ -1,11 +1,8 @@
 #!/usr/bin/env bash
 set -o errexit
 
-# Remove any existing virtual environment
-rm -rf .venv
-
 # Upgrade pip
 pip install --break-system-packages --upgrade pip
 
-# Force reinstall all packages
-pip install --break-system-packages --force-reinstall --no-cache-dir -r requirements.txt
+# Install all packages
+pip install --break-system-packages --no-cache-dir -r requirements.txt
